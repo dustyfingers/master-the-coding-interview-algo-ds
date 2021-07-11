1597bf714a06b04f88355b77a4adff6a474b28c5
lou <dustyfingers01257@gmail.com>
2021-07-10 19:11:10 -0600

    learned about gauging code performance with nodejs

--- 1-big-o/good-code.js
+++ 1-big-o/good-code.js
@@ -1,9 +1,14 @@
-const nemo = ['nemo']
+const { performance } = require('perf_hooks')
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
+const large = new Array(10000).fill('nemo')
 
 function findNemo(array) {
+    let t0 = performance.now()
     for (let i = 0; i < array.length; i++) {
         if (array[i] === 'nemo') console.log('found nemo!')
     }
+    let t1 = performance.now()
+    console.log(`Call to findNemo took ${t1-t0}ms`)
 }
 
-findNemo(nemo)
+findNemo(large)
