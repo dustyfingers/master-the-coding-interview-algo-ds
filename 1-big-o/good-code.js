8e0b488b074209d59b83d59434b743786f8591cc
lou <dustyfingers01257@gmail.com>
2021-07-12 08:45:31 -0600

    learned about constant runtime

--- 1-big-o/good-code.js
+++ 1-big-o/good-code.js
@@ -5,10 +5,16 @@ const large = new Array(10000).fill('nemo')
 function findNemo(array) {
     // there are n operations performed in this implementation,
     //  where n in this case is the length of the searched array
-    // this is also called 'linear time
+    // this is also called 'linear time'
     for (let i = 0; i < array.length; i++) {
         if (array[i] === 'nemo') console.log('found nemo!')
     }
 }
 
 findNemo(large)
+
+function compressFirstBox(boxes) {
+    // as 'boxes' grows, the # of operations remains constant
+    // known as 'constant time'
+    console.log(boxes[0]);
+}
