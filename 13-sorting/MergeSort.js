60c3a2d52e8192d429778e0a7ba7d083fe125e68
Louie <dustyfingers01257@gmail.com>
2024-12-13 10:57:43 -0700

    learn about merge sort

---
+++ 13-sorting/MergeSort.js
@@ -0,0 +1,10 @@
+// merge sort splits the array into smaller arrays again and again
+
+const nums = [2, 65, 244, 32, 1, 4, 7, 8, 357, 44, 9];
+
+// sort items least to greatest using merge sort
+const mergeSort = (arr) => {
+  return arr;
+};
+
+console.log(mergeSort(nums));
--- 13-sorting/notes.md
+++ 13-sorting/notes.md
@@ -1,2 +1,5 @@
 sorting comes up a ton during interviews!
 
+insertion sort - great for small or mostly sorted data sets
+
+merge sort and quick sort - they use the divide and conquer method
